dec45129c207a1fda766afa94ae78a7bb91b39a1
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-08 12:01:42 +0530

    First commit of Anagram check

--- src/AnagramCheck.java
+++ src/AnagramCheck.java
@@ -1,10 +1,27 @@
+import java.util.*;
 public class AnagramCheck {
-   
-        // TODO: Read the two strings
-        // TODO: Convert both to lowercase (to handle case-insensitivity)
-        // TODO: Check if lengths are equal. If not, print "false" and return.
-        // TODO: Convert strings to char arrays
-        // TODO: Sort both char arrays
-        // TODO: Compare the sorted arrays. Print "true" if equal, else "false".
-  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s1 = sc.nextLine().toLowerCase();
+        String s2 = sc.nextLine().toLowerCase();
+
+        // If lengths are different, not anagrams
+        if (s1.length() != s2.length()) {
+            System.out.print("false");
+            return;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
 }
